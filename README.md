21584cfb3ffa4fbb368a496f2d314bec4f2d933b
Alessandro De Blasis <Alessandro.DeBlasis@clarksons.com>
2018-08-15 11:23:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# React Code Challenge
+
+You can implement this in your favourite way, we are interested in the approach more than anything else.
+Feel free to install any package you believe is necessary to achieve the final goal if necessary.
+Please vocalise your thoughts and ask questions if needed.
+
+# ✅ Tasks:
+- Please create a component called "**ClkContainer**"
+- Please create a new component called "**ClkSelectList**"
+  Its purpose is to wrap the `<select>` HTML element and to bind it to a model. 
+  The available options are `["white", "red", "green", "yellow", "blue"]`
+- Please create a component called "**ClkInputSelector**" Its purpose is to wrap the `<input>` HTML element and to add some logic to it.
+
+# 🏁 The final goal is:
+- Display **ClkSelectList** with all its options
+- Display **ClkInputSelector** next to it
+- If the content of **ClkInputSelector** matches one of the available options in **ClkSelectList**,   select that value, if not, don't do anything
+- **BONUS**: provide a way for adding new elements to the **ClkSelectList** at runtime while preserving the functionality you have built so far, again, you have complete freedom over the approach
+
+------------------
---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React Code Challenge</h1>
+        </header>
+        <p className="App-intro">
+
+          You can implement this in your favourite way, we are interested in the approach more than anything else.<br />
+          Feel free to install any package you believe is necessary to achieve the final goal if necessary.<br />
+
+          Please vocalise your thoughts and ask questions if needed.
+        </p>
+        <p className="App-intro">
+          <h1>Task:</h1>
+          <ul>
+            <li>Please create a component called "<strong>ClkContainer</strong>"</li>
+            <li>Please create a new component called "<strong>ClkSelectList</strong>"<br />
+          Its purpose is to wrap the &lt;select&gt; HTML element and to bind it to a model. <br />
+          The available options are ["white", "red", "green", "yellow", "blue"]
+          </li>
+
+          <li>Please create a component called "<strong>ClkInputSelector</strong>"
+          Its purpose is to wrap the &lt;input&gt; HTML element and to add some logic to it.</li>
+          </ul>
+       
+          The final goal is:
+          <ul>
+          <li>Display <strong>ClkSelectList</strong> with all its options</li>
+          <li>Display <strong>ClkInputSelector</strong> next to it</li>
+          <li>If the content of <strong>ClkInputSelector</strong> matches one of the available options in <strong>ClkSelectList</strong>, select that value, if not, don't do anything </li>
+          <li>BONUS: provide a way for adding new elements to the <strong>ClkSelectList</strong> at runtime at runtime while preserving the functionality you have built so far, again, you have complete freedom over the approach</li>
+          </ul>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
